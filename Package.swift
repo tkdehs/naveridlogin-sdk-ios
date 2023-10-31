27dd6519e80c131c313ac971e475e3163ef061c0
Sangdon Kim <107008333+sangdon-kim-switchwon@users.noreply.github.com>
2023-10-31 13:59:29 +0900

    변경

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "NaverThirdPartyLogin",
-            url: "https://github.com/jaemyeong/naveridlogin-sdk-ios-spm/releases/download/4.1.5/NaverThirdPartyLogin.xcframework.zip",
+            url: "https://github.com/tkdehs/naveridlogin-sdk-ios-spm/releases/download/4.1.5/NaverThirdPartyLogin.xcframework.zip",
             checksum: "43463dcfc323da9dc7afb0c472976e632822c47ead313382f098720869ce2b26"
         ),
     ]
